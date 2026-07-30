188708ea4ae5814aa9c9df4c2f5edf96304f6c93
KHUSHAL-VAISHNAV-K1 <khushbairagi7@gmail.com>
2026-07-31 01:31:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (66.43%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -1,20 +1,28 @@
 class Solution {
     public int minimumPushes(String word) {
         
-        HashMap<Character,Integer> map = new HashMap<>();
-        for (char ch : word.toCharArray()){
-            map.put(ch, map.getOrDefault(ch,0)+1);
-        }
-        List<Integer> freqList = new ArrayList<>(map.values());
-        Collections.sort(freqList);
-        Collections.reverse(freqList);
+        // HashMap<Character,Integer> map = new HashMap<>();
+        // for (char ch : word.toCharArray()){
+        //     map.put(ch, map.getOrDefault(ch,0)+1);
+        // }
+        // List<Integer> freqList = new ArrayList<>(map.values());
+        // Collections.sort(freqList);
+        // Collections.reverse(freqList);
 
-        int totalpushes= 0 ; 
-        int presscount = 0 ; 
-        for (int i = 0 ; i < freqList.size();i++){
-            presscount = (i/8)+1;
-            totalpushes += freqList.get(i)*presscount; 
-        }
-        return totalpushes;
+        // int totalpushes= 0 ; 
+        // int presscount = 0 ; 
+        // for (int i = 0 ; i < freqList.size();i++){
+        //     presscount = (i/8)+1;
+        //     totalpushes += freqList.get(i)*presscount; 
+        // }
+        // return totalpushes;
+   
+int ans = 0 ;
+    for(int i = 0 ; i < word.length();i++){
+         ans += (i/8)+1;
+
+    }return ans ; 
+   
     }
+
 }
